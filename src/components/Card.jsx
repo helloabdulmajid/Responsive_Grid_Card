0df31875d30855c0cf1ccd3a68c2c15cd278c774
Majid <helloabdulmajid>
2022-08-09 13:25:30 +0530

    add card & card list

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,32 +9,35 @@ const Card = () => {
    <>
     <div className="main">
         <h1>Responsive Card Grid Layout</h1>
-        
+        <ul className="cards">
+                        
+                      
 
     
         {
                 CardList.map((x)=>{
                     return(
                         <>
-                        <ul className="cards">
                          <li className="cards_item">
-                         <div className="card">
-                        
-                        <div className="card_image">{x.image}</div>
-                      
+                           <div className="card">
+                        <div className="card_image"><img   src={x.image} alt="am"/>
+                        </div>
+
                   <div className="card_content">
                 <h2 className="card_title">{x.title}</h2>
                 <p className="card_text">{x.text}</p>
                 <button className="btn card_btn">{x.more}</button>
               </div>
               </div>
-         
-              </li>
-              </ul>
+         </li>
+             
               </>
                     );
                 })
             }
+             
+         
+         </ul>
 
       
     </div>
